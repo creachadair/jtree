127bba31f1364dbf910de9b562a4b44a43799980
M. J. Fromberger <michael.j.fromberger@gmail.com>
2021-12-01 14:06:16 -0800

    Add the Stream parser, Anchor, and Handler types.

---
+++ stream.go
@@ -0,0 +1,231 @@
+package jtree
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+// An Anchor represents a location in source text. The methods of an Anchor
+// will report the location, token type, and contents of the anchor.
+type Anchor interface {
+	Location() Location // Returns the source location of the anchor
+	Token() Token       // Returns the token type of the anchor
+	Text() string       // Returns the unescaped text of the anchor
+	Unescape() string   // Unescapes the text of the anchor
+}
+
+// A Handler handles events from parsing an input stream.  If a method reports
+// an error, parsing stops and that error is returned to the caller.
+// The parser ensures objects and arrays are correctly balanced.
+//
+// The Anchor argument to a Handler method is only valid for the duration of
+// that method call. If the method needs to retain information about the
+// location after it returns, it must copy the relevant data.
+//
+// The SyntaxError method is special: The parser calls it to report a syntax
+// error, and terminates parsing regardless what it returns.
+type Handler interface {
+	// Begin a new object, whose open brace is at loc.
+	BeginObject(loc Anchor) error
+
+	// End the most-recently-opened object, whose close brace is at loc.
+	EndObject(loc Anchor) error
+
+	// Begin a new array, whose open bracket is at loc.
+	BeginArray(loc Anchor) error
+
+	// End the most-recently-opened array, whose close bracket is at loc.
+	EndArray(loc Anchor) error
+
+	// Begin a new object member, whose key is at loc.
+	BeginMember(loc Anchor) error
+
+	// End the current object member giving the location and type of the token
+	// that terminated the member (either Comma or RBrace).
+	EndMember(loc Anchor) error
+
+	// Report a data value at the given location.
+	Value(loc Anchor) error
+
+	// Report a syntax error at the given location and err != nil.
+	// If SyntaxError returns a non-nil error, that value replaces err and is
+	// reported from the parser. Otherwise the parser exits reporting err.
+	SyntaxError(loc Anchor, err error) error
+
+	// EndOfInput reports the end of the input stream.
+	EndOfInput(loc Anchor)
+}
+
+// Stream is a stream parser that consumes input and delivers events to a
+// Handler corresponding with the structure of the input.
+type Stream struct {
+	s *Scanner
+}
+
+// NewStream constructs a new Stream that consumes input from r.
+func NewStream(r io.Reader) *Stream { return &Stream{s: NewScanner(r)} }
+
+func (s *Stream) recoverParseError(errp *error) {
+	if serr := recover(); serr != nil {
+		*errp = serr.(error)
+	}
+}
+
+// Parse parses the input stream and delivers events to h until either an error
+// occurs or the input is exhausted.
+func (s *Stream) Parse(h Handler) (err error) {
+	defer s.recoverParseError(&err)
+
+	for {
+		err := s.s.Next()
+		if err == io.EOF {
+			h.EndOfInput(s.s)
+			return nil
+		} else if err != nil {
+			s.syntaxError(h, "invalid input: %w", err)
+		}
+
+		s.parseElement(h)
+	}
+}
+
+// ParseOne parses a single value from the input stream and delivers events to
+// h until the value is complete or an error occurs. If no further value is
+// available from the input, ParseOne returns io.EOF.
+func (s *Stream) ParseOne(h Handler) (err error) {
+	defer s.recoverParseError(&err)
+
+	if err := s.s.Next(); err == io.EOF {
+		h.EndOfInput(s.s)
+		return err
+	} else if err != nil {
+		s.syntaxError(h, "invalid input: %w", err)
+	}
+	s.parseElement(h)
+	return nil
+}
+
+// parseElement consumes a single value of any type.
+// Precondition: token != Invalid.
+func (s *Stream) parseElement(h Handler) {
+	switch tok := s.s.Token(); tok {
+	case LBrace:
+		s.checkError(h.BeginObject(s.s))
+		s.parseMembers(h)
+		s.require(h, RBrace)
+		s.checkError(h.EndObject(s.s))
+	case LSquare:
+		s.checkError(h.BeginArray(s.s))
+		s.parseElements(h)
+		s.require(h, RSquare)
+		s.checkError(h.EndArray(s.s))
+	case Integer, Number, String, True, False, Null:
+		s.checkError(h.Value(s.s))
+	case RBrace, RSquare, Comma, Colon:
+		s.syntaxError(h, "unexpected %v", tok)
+	default:
+		s.syntaxError(h, "unknown token %v", tok)
+	}
+}
+
+// parseMembers consumes zero of more key:value object members.
+// Precondition: token == LBrace.
+// Postcondition: token == RBrace.
+func (s *Stream) parseMembers(h Handler) {
+	tok := s.advance(h, RBrace, String)
+	if tok == RBrace {
+		return // end of object
+	}
+	for {
+		// Parse a single member: "key": value
+		s.checkError(h.BeginMember(s.s))
+		s.advance(h, Colon)
+		s.advance(h)
+		s.parseElement(h)
+
+		// Check whether we have more members (",") or are done ("}").
+		tok := s.advance(h, RBrace, Comma)
+		if tok == RBrace {
+			s.checkError(h.EndMember(s.s))
+			return // end of object
+		}
+		s.checkError(h.EndMember(s.s))
+		s.advance(h, String) // advance to next key
+	}
+}
+
+// parseElements consumes zero or more comma-separated array values.
+// Precondition: token == LSquare.
+// Postcondition: token == RSquare.
+func (s *Stream) parseElements(h Handler) {
+	if tok := s.advance(h); tok == RSquare {
+		return // end of array
+	}
+	s.parseElement(h)
+	for {
+		tok := s.advance(h, RSquare, Comma)
+		if tok == RSquare {
+			return // end of array
+		}
+		s.advance(h)
+		s.parseElement(h)
+	}
+}
+
+func (s *Stream) advance(h Handler, tokens ...Token) Token {
+	if err := s.s.Next(); err != nil {
+		s.syntaxError(h, "expected %v, got error: %w", tokLabel(tokens), err)
+	}
+	tok := s.s.Token()
+	if len(tokens) != 0 && !tokOneOf(tok, tokens) {
+		s.syntaxError(h, "expected %v, got %v", tokLabel(tokens), tok)
+	}
+	return tok
+}
+
+func (s *Stream) require(h Handler, token Token) {
+	if tok := s.s.Token(); tok != token {
+		s.syntaxError(h, "expected %v, got %v", token, tok)
+	}
+}
+
+func (s *Stream) syntaxError(h Handler, msg string, args ...interface{}) {
+	loc := s.s.Location().First
+	err := fmt.Errorf("at %s: "+msg, append([]interface{}{loc}, args...)...)
+	if herr := h.SyntaxError(s.s, err); herr != nil {
+		panic(herr)
+	}
+	panic(err)
+}
+
+func (s *Stream) checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// tokLabel makes a human-readable summary string for the given token types.
+func tokLabel(tokens []Token) string {
+	if len(tokens) == 0 {
+		return "more input"
+	} else if len(tokens) == 1 {
+		return tokens[0].String()
+	}
+	last := len(tokens) - 1
+	ss := make([]string, len(tokens)-1)
+	for i, tok := range tokens[:last] {
+		ss[i] = tok.String()
+	}
+	return strings.Join(ss, ", ") + " or " + tokens[last].String()
+}
+
+// tokOneOf reports whether cur is an element of tokens.
+func tokOneOf(cur Token, tokens []Token) bool {
+	for _, token := range tokens {
+		if cur == token {
+			return true
+		}
+	}
+	return false
+}
---
+++ stream_test.go
@@ -0,0 +1,218 @@
+package jtree_test
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/creachadair/jtree"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestStream(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{"", "."},
+		{"   ", "."},
+
+		{"true false null", `
+Value true "true"
+Value false "false"
+Value null "null"
+.`},
+
+		{`0 5 -6.32 0.1e-2`, `
+Value integer "0"
+Value integer "5"
+Value number "-6.32"
+Value number "0.1e-2"
+.`},
+
+		{`"" "a b c" "a\tb" "a\u0020b"`, `
+Value string ""
+Value string "a b c"
+Value string "a\tb"
+Value string "a\u0020b"
+.`},
+
+		{`{}`, "BeginObject\nEndObject\n."},
+
+		{`{"a":15}`, `
+BeginObject
+BeginMember "a"
+Value integer "15"
+EndMember "}"
+EndObject
+.`},
+
+		{`{"x":null, "y":[true]}`, `
+BeginObject
+BeginMember "x"
+Value null "null"
+EndMember ","
+BeginMember "y"
+BeginArray
+Value true "true"
+EndArray
+EndMember "}"
+EndObject
+.`},
+
+		{`[]`, "BeginArray\nEndArray\n."},
+	}
+
+	for _, test := range tests {
+		st := jtree.NewStream(strings.NewReader(test.input))
+		th := new(testHandler)
+		if err := st.Parse(th); err != nil {
+			t.Errorf("Parse failed: %v", err)
+		}
+
+		if diff := diffStrings(test.want, th.output()); diff != "" {
+			t.Errorf("Input: %#q\nOutput: (-want, +got)\n%s", test.input, diff)
+		}
+	}
+}
+
+func TestStreamErrors(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		// Various kinds of unbalanced object bits.
+		{`{`, `
+BeginObject
+SyntaxError at 1:1: expected "}" or string, got error: EOF`},
+		{`}`, `
+SyntaxError at 1:0: unexpected "}"`},
+		{`{false:1}`, `
+BeginObject
+SyntaxError at 1:1: expected "}" or string, got false`},
+		{`{"true":}`, `
+BeginObject
+BeginMember "true"
+SyntaxError at 1:8: unexpected "}"`},
+		{`{"true":1,`, `
+BeginObject
+BeginMember "true"
+Value integer "1"
+EndMember ","
+SyntaxError at 1:10: expected string, got error: EOF`},
+
+		// Unbalanced array bits.
+		{`[`, `
+BeginArray
+SyntaxError at 1:1: expected more input, got error: EOF`},
+		{`]`, `
+SyntaxError at 1:0: unexpected "]"`},
+		{`[15,`, `
+BeginArray
+Value integer "15"
+SyntaxError at 1:4: expected more input, got error: EOF`},
+		{`[15,]`, `
+BeginArray
+Value integer "15"
+SyntaxError at 1:4: unexpected "]"`},
+
+		// Invalid values.
+		{`1 2.0 forthright`, `
+Value integer "1"
+Value number "2.0"
+SyntaxError at 1:6: invalid input: offset 16: unknown constant "forthright"`},
+		{`"what did you`, `
+SyntaxError at 1:0: invalid input: offset 13: unexpected error: EOF`},
+	}
+
+	for _, test := range tests {
+		st := jtree.NewStream(strings.NewReader(test.input))
+		th := new(testHandler)
+		if err := st.Parse(th); err == nil {
+			t.Error("Parse did not report an error")
+		}
+
+		if diff := diffStrings(test.want, th.output()); diff != "" {
+			t.Errorf("Input: %#q\nOutput: (-want, +got)\n%s", test.input, diff)
+		}
+	}
+}
+
+func TestParseOne(t *testing.T) {
+	const input = `{ "love": true } [] "ok"`
+	const want = `
+BeginObject
+BeginMember "love"
+Value true "true"
+EndMember "}"
+EndObject
+---
+BeginArray
+EndArray
+---
+Value string "ok"
+---
+.`
+	th := new(testHandler)
+
+	st := jtree.NewStream(strings.NewReader(input))
+	for {
+		err := st.ParseOne(th)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			t.Fatalf("ParseOne failed: %v", err)
+		}
+		th.pr("---")
+	}
+
+	if diff := diffStrings(want, th.output()); diff != "" {
+		t.Errorf("Input: %#q\nOutput: (-want, +got)\n%s", input, diff)
+	}
+}
+
+func diffStrings(want, got string) string {
+	return cmp.Diff(strings.Split(strings.TrimSpace(want), "\n"),
+		strings.Split(strings.TrimSpace(got), "\n"))
+}
+
+type testHandler struct {
+	buf bytes.Buffer
+}
+
+func (t *testHandler) pr(msg string, args ...interface{}) {
+	if !strings.HasSuffix(msg, "\n") {
+		msg += "\n"
+	}
+	fmt.Fprintf(&t.buf, msg, args...)
+}
+
+func (t *testHandler) output() string { return t.buf.String() }
+
+func (t *testHandler) BeginObject(loc jtree.Anchor) error { t.pr("BeginObject"); return nil }
+func (t *testHandler) EndObject(loc jtree.Anchor) error   { t.pr("EndObject"); return nil }
+func (t *testHandler) BeginArray(loc jtree.Anchor) error  { t.pr("BeginArray"); return nil }
+func (t *testHandler) EndArray(loc jtree.Anchor) error    { t.pr("EndArray"); return nil }
+func (t *testHandler) EndOfInput(loc jtree.Anchor)        { t.pr(".") }
+
+func (t *testHandler) BeginMember(loc jtree.Anchor) error {
+	t.pr("BeginMember %q", loc.Text())
+	return nil
+}
+
+func (t *testHandler) EndMember(loc jtree.Anchor) error {
+	t.pr("EndMember %s", loc.Token())
+	return nil
+}
+
+func (t *testHandler) Value(loc jtree.Anchor) error {
+	t.pr(`Value %s "%s"`, loc.Token(), loc.Text())
+	return nil
+}
+
+func (t *testHandler) SyntaxError(loc jtree.Anchor, err error) error {
+	t.pr("SyntaxError %v", err)
+	return nil
+}
