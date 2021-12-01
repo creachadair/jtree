88e402fd979f6671a179966b893fc385c9e5e6ce
M. J. Fromberger <michael.j.fromberger@gmail.com>
2021-12-01 08:21:57 -0800

    Add basic scanner benchmarks.

---
+++ bench_test.go
@@ -0,0 +1,47 @@
+package jtree_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/creachadair/jtree"
+)
+
+func BenchmarkScanner(b *testing.B) {
+	input, err := os.ReadFile("testdata/input.json")
+	if err != nil {
+		b.Fatalf("Reading test input: %v", err)
+	}
+	b.Logf("Benchmark input: %d bytes", len(input))
+
+	b.Run("Decoder", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			dec := json.NewDecoder(bytes.NewReader(input))
+			for {
+				_, err := dec.Token()
+				if err == io.EOF {
+					break
+				} else if err != nil {
+					b.Fatalf("Unexpected error: %v", err)
+				}
+			}
+		}
+	})
+
+	b.Run("Scanner", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			dec := jtree.NewScanner(bytes.NewReader(input))
+			for {
+				err := dec.Next()
+				if err == io.EOF {
+					break
+				} else if err != nil {
+					b.Fatalf("Unexpected error: %v", err)
+				}
+			}
+		}
+	})
+}
