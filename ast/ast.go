888f81227966ba8b223f0df65783c614c7984daa
M. J. Fromberger <michael.j.fromberger@gmail.com>
2021-12-02 11:22:59 -0800

    ast: add constructors for value token types

--- ast/ast.go
+++ ast/ast.go
@@ -76,25 +76,43 @@ func (d datum) astValue() {}
 func (d datum) Text() string { return string(d.text) }
 
 // An Integer is an integer value.
-type Integer struct{ datum }
+type Integer struct {
+	datum
+	value *int64
+}
 
-func (z Integer) Int64() int64 {
-	v, err := strconv.ParseInt(string(z.text), 10, 64)
-	if err != nil {
-		panic(err)
+// NewInteger constructs an Integer token with the given value.
+func NewInteger(z int64) *Integer { return &Integer{value: &z} }
+
+func (z *Integer) Int64() int64 {
+	if z.value == nil {
+		v, err := strconv.ParseInt(string(z.text), 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		z.value = &v
 	}
-	return v
+	return *z.value
 }
 
 // A Number is a floating-point value.
-type Number struct{ datum }
+type Number struct {
+	datum
+	value *float64
+}
 
-func (n Number) Float64() float64 {
-	v, err := strconv.ParseFloat(string(n.text), 64)
-	if err != nil {
-		panic(err)
+// NewNumber constructs a Number token with the given value.
+func NewNumber(f float64) *Number { return &Number{value: &f} }
+
+func (n *Number) Float64() float64 {
+	if n.value == nil {
+		v, err := strconv.ParseFloat(string(n.text), 64)
+		if err != nil {
+			panic(err)
+		}
+		n.value = &v
 	}
-	return v
+	return *n.value
 }
 
 // A Bool is a Boolean constant, true or false.
@@ -103,17 +121,29 @@ type Bool struct {
 	value bool
 }
 
+// NewBool constructs a Bool token with the given value.
+func NewBool(v bool) *Bool { return &Bool{value: v} }
+
 func (b Bool) Value() bool { return b.value }
 
 // A String is a string value.
-type String struct{ datum }
+type String struct {
+	datum
+	unescaped []byte
+}
+
+// NewString constructs a String token with the given unescaped value.
+func NewString(s string) *String { return &String{unescaped: []byte(s)} }
 
 func (s String) Unescape() string {
-	dec, err := jtree.Unescape(s.text)
-	if err != nil {
-		panic(err)
+	if s.unescaped == nil && len(s.text) != 0 {
+		dec, err := jtree.Unescape(s.text)
+		if err != nil {
+			panic(err)
+		}
+		s.unescaped = dec
 	}
-	return string(dec)
+	return string(s.unescaped)
 }
 
 // Null represents the null constant.
