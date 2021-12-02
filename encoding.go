12858a5a0e448484179aa2e3bb7da9e98205dfc7
M. J. Fromberger <michael.j.fromberger@gmail.com>
2021-12-01 20:13:54 -0800

    encode: unescape as bytes

--- encoding.go
+++ encoding.go
@@ -5,38 +5,48 @@ package jtree
 import (
 	"bytes"
 	"errors"
-	"strconv"
-	"strings"
+	"fmt"
 	"unicode/utf8"
 )
 
-// UnescapeString decodes the JSON encoding of a string. The input must have
-// the enclosing double quotation marks already removed. Escape sequences are
-// replaced with their unescaped equivalents. Invalid escapes are replaced by
-// the Unicode replacement rune. DecodeString reports an error for an
-// incomplete escape sequence.
+// UnescapeString decodes the JSON encoding of a string. This function is a
+// shorthand for a type conversion and a call to UnescapeBytes.
 func UnescapeString(src string) (string, error) {
-	if !strings.ContainsRune(src, '\\') {
+	u, err := Unescape([]byte(src))
+	if err != nil {
+		return "", err
+	}
+	return string(u), nil
+}
+
+// Unescape decodes a byte slice containing the JSON encoding of a string. The
+// input must have the enclosing double quotation marks already removed.
+//
+// Escape sequences are replaced with their unescaped equivalents. Invalid
+// escapes are replaced by the Unicode replacement rune. DecodeString reports
+// an error for an incomplete escape sequence.
+func Unescape(src []byte) ([]byte, error) {
+	if !bytes.ContainsRune(src, '\\') {
 		return src, nil
 	}
 
 	dec := bytes.NewBuffer(make([]byte, 0, len(src)))
-	for src != "" {
-		i := strings.IndexRune(src, '\\')
+	for len(src) != 0 {
+		i := bytes.IndexRune(src, '\\')
 		if i < 0 {
-			dec.WriteString(src)
+			dec.Write(src)
 			break
 		}
-		dec.WriteString(src[:i])
+		dec.Write(src[:i])
 
 		// Decode the next rune after the escape to figure out what to
 		// substitute. There should not be errors here, but if there are, insert
 		// replacement runes (utf8.RuneError == '\ufffd').
 		src = src[i+1:]
-		if src == "" {
-			return "", errors.New("incomplete escape sequence")
+		if len(src) == 0 {
+			return nil, errors.New("incomplete escape sequence")
 		}
-		r, n := utf8.DecodeRuneInString(src)
+		r, n := utf8.DecodeRune(src)
 		if n == 0 {
 			n++
 		}
@@ -57,9 +67,9 @@ func UnescapeString(src string) (string, error) {
 			dec.WriteRune('\t')
 		case 'u':
 			if len(src) < 4 {
-				return "", errors.New("incomplete Unicode escape")
+				return nil, errors.New("incomplete Unicode escape")
 			}
-			v, err := strconv.ParseInt(src[:4], 16, 64)
+			v, err := parseHex(src[:4])
 			if err != nil {
 				dec.WriteRune(utf8.RuneError)
 			} else {
@@ -70,5 +80,22 @@ func UnescapeString(src string) (string, error) {
 			dec.WriteRune(utf8.RuneError)
 		}
 	}
-	return dec.String(), nil
+	return dec.Bytes(), nil
+}
+
+func parseHex(data []byte) (int64, error) {
+	var v int64
+	for _, b := range data {
+		v <<= 4
+		if '0' <= b && b <= '9' {
+			v += int64(b - '0')
+		} else if 'a' <= b && b <= 'f' {
+			v += int64(b - 'a' + 10)
+		} else if 'A' <= b && b <= 'F' {
+			v += int64(b - 'A' + 10)
+		} else {
+			return 0, fmt.Errorf("invalid hex digit %q", b)
+		}
+	}
+	return v, nil
 }
