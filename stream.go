0ea0b23188c4ca81a2f759f5a9fdd134341d5ebc
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-04-26 10:18:34 -0700

    Fix a documentation bug.

--- stream.go
+++ stream.go
@@ -13,7 +13,7 @@ import (
 type Anchor interface {
 	Span() Span   // Returns the source span of the anchor
 	Token() Token // Returns the token type of the anchor
-	Text() []byte // Returns the unescaped text of the anchor
+	Text() []byte // Returns the raw (undecoded) text of the anchor
 }
 
 // A Handler handles events from parsing an input stream.  If a method reports
