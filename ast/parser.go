2d9f53c9de97fc44d7ff7ac6de065a0342190bb7
M. J. Fromberger <michael.j.fromberger@gmail.com>
2023-01-06 16:11:48 -0800

    ast: reduce allocation in the parser
    Instead of keeping a stack of pointers, keep the values and only take the
    address of the value when we need to.
    
      name                       old time/op    new time/op    delta
      Scanner/JTree/Scanner-10     1.47ms ± 0%    1.47ms ± 0%   +0.11%
      Scanner/JTree/Stream-10      1.52ms ± 0%    1.52ms ± 0%   +0.25%
      Scanner/JTree/ParseAST-10    2.17ms ± 0%    2.22ms ± 0%   +2.12%
    
      name                       old alloc/op   new alloc/op   delta
      Scanner/JTree/Scanner-10     7.70kB ± 0%    7.70kB ± 0%     ~
      Scanner/JTree/Stream-10      7.93kB ± 0%    7.93kB ± 0%     ~
      Scanner/JTree/ParseAST-10     809kB ± 0%     727kB ± 0%  -10.17%
    
      name                       old allocs/op  new allocs/op  delta
      Scanner/JTree/Scanner-10        580 ± 0%       580 ± 0%     ~
      Scanner/JTree/Stream-10         583 ± 0%       583 ± 0%     ~
      Scanner/JTree/ParseAST-10     22.7k ± 0%     20.8k ± 0%   -8.50%

--- ast/parser.go
+++ ast/parser.go
@@ -29,7 +29,7 @@ func (p *Parser) Parse() (Value, error) {
 	} else if len(p.h.stk) != 1 {
 		return nil, errors.New("incomplete value")
 	}
-	out := *p.h.stk[0]
+	out := p.h.stk[0]
 	p.h.stk = p.h.stk[:0]
 	return out, nil
 }
@@ -53,7 +53,7 @@ func Parse(r io.Reader) ([]Value, error) {
 // A parseHandler implements the jtree.Handler interface to construct abstract
 // syntax trees for JSON values.
 type parseHandler struct {
-	stk  []*Value
+	stk  []Value
 	tbuf [][]byte
 }
 
@@ -89,7 +89,8 @@ func (h *parseHandler) copyOf(text []byte) []byte {
 	return h.tbuf[i][s : s+len(text)]
 }
 
-func merge(old *Value, v Value) {
+func (h *parseHandler) mergeTop(v Value) {
+	old := &h.stk[len(h.stk)-1]
 	switch t := (*old).(type) {
 	case *Member:
 		t.Value = v
@@ -103,27 +104,25 @@ func merge(old *Value, v Value) {
 func (h *parseHandler) reduce() error {
 	if len(h.stk) > 1 {
 		v := h.pop()
-		merge(h.top(), v)
+		h.mergeTop(v)
 	}
 	return nil
 }
 
 func (h *parseHandler) reduceValue(v Value) error {
 	if len(h.stk) > 0 {
-		merge(h.top(), v)
+		h.mergeTop(v)
 	}
 	return nil
 }
 
-func (h *parseHandler) top() *Value { return h.stk[len(h.stk)-1] }
-
 func (h *parseHandler) pop() Value {
-	last := h.top()
+	last := h.stk[len(h.stk)-1]
 	h.stk = h.stk[:len(h.stk)-1]
-	return *last
+	return last
 }
 
-func (h *parseHandler) push(v Value) { h.stk = append(h.stk, &v) }
+func (h *parseHandler) push(v Value) { h.stk = append(h.stk, v) }
 
 func (h *parseHandler) BeginObject(loc jtree.Anchor) error {
 	h.push(Object(nil))
@@ -149,7 +148,7 @@ func (h *parseHandler) BeginMember(loc jtree.Anchor) error {
 	// stack after the value is known, we don't have to reduce multiple times.
 
 	mem := &Member{Key: Quoted{text: h.copyOf(loc.Text())}}
-	top := h.top()
+	top := &h.stk[len(h.stk)-1]
 	obj := (*top).(Object)
 	*top = append(obj, mem)
 	h.push(mem)
